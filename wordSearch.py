038e5e6927417bd76db688e5e398b250974443d2
AnassBousseaden <anassbousseaden@yahoo.fr>
2021-12-30 22:01:36 +0100

    wordSearch and rename MinmumWindowSunString

--- wordSearch.py
+++ wordSearch.py
@@ -1,51 +1,33 @@
-from typing import overload
-
-
 class Solution:
-    def isValidWindow(self, s: str, H_t: dict, keys: set) -> bool:
-        H_s = dict.fromkeys(keys, 0)
-        for char in s:
-            if H_s.get(char) == None:
-                continue
-            H_s[char] += 1
-        for k in keys:
-            if H_s[k] < H_t[k]:
-                return False
-        return True
+    def dfs(self, m, n, i, j, board, visited, word):
+        if word == "" or (board[i][j] == word[0] and len(word) == 1):
+            return True
+        if board[i][j] != word[0]:
+            return False
+        visited[i][j] = True
+        print(i, j, board[i][j], visited)
+        for dir in [-1, 1]:
+            if 0 <= i+dir and i+dir < m and not visited[i+dir][j] and self.dfs(m, n, i+dir, j, board, visited, word[1:]):
+                return True
+            if 0 <= j+dir and j+dir < n and not visited[i][j+dir] and self.dfs(m, n, i, j+dir, board, visited, word[1:]):
+                return True
+        visited[i][j] = False
+        return False
 
-    def minWindow(self, s: str, t: str) -> str:
-        n = len(s)
-        bestWindow = ""
-        bestlength = n+1
-        i, j = 0, 0
-        # create the Dic associated with t
-        keys = set(t)
-        H = dict.fromkeys(keys, 0)
-        for k in keys:
-            H[k] = t.count(k)
-
-        while i < n or j < n:
-            if i == j:
-                j = j+1
-            else:
-                tmp = s[i:j]
-                print(i, j, tmp)
-                if self.isValidWindow(tmp, H, keys):
-                    if bestlength > j-i:
-                        bestWindow = tmp
-                        bestlength = j-i
-                    i = i + 1
-                else:
-                    if j < n:
-                        j = j+1
-                    else:
-                        i = i+1
-        return bestWindow
+    def exist(self, board: list[list[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+        visited = [[False]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if self.dfs(m, n, i, j, board, visited, word):
+                    return True
+        return False
 
 
 test = Solution()
 
-s = "ADOBECODEBANC"
-t = "ABC"
+board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+word = "ABCCED"
 
-print(test.minWindow(s, t))
+print(test.exist(board, word))
